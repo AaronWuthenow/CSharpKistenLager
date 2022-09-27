1383848ab31b5ee12bbec5f646900c2f2d69020f
Aaron Wuthenow <aaron.wuthenow@gobas.de>
2022-09-27 09:46:27 +0200

    Added support for showing boxes.

---
+++ Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lagersystem
+{
+    internal class Menu
+    {
+        static public void MainMenu()
+        {
+            Console.WriteLine("1. Create a Box");
+            Console.WriteLine("2. Change a Box");
+            Console.WriteLine("3. Delete a Box");
+            Console.WriteLine("4. Show a Box");
+            Console.WriteLine("5. Show all Boxes");
+            Console.WriteLine("6. Quit");
+        }
+
+        static public void Options()
+        {
+            Console.WriteLine("1. Search Box with ID");
+            Console.WriteLine("2. Search Box with Index");
+            Console.WriteLine("3. Back to main menu");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,28 +15,40 @@ namespace Lagersystem
             public int Width;
             public int Length;
             public int Volume;
+            public int Location;
 
-            public Box(int boxID, int boxHeight, int boxWidth, int boxLength)
+            public Box(int boxID, int boxHeight, int boxWidth, int boxLength, int location)
             {
                 this.ID = boxID;
                 this.Height = boxHeight;
                 this.Width = boxWidth;
                 this.Length = boxLength;
                 this.Volume = this.Width * this.Height * this.Length;
+                this.Location = location;
+            }
+            public Box(int boxID = 0, int location = 0)
+            {
+                this.ID = boxID;
+                this.Height = 0;
+                this.Width = 0;
+                this.Length = 0;
+                this.Volume = 0;
+                this.Location = location;
             }
         };
 
         static void showBox(Box inBox)
         {
-            Console.Write($"\nID: {inBox.ID}\n" +
+            Console.Write($"\nID: {inBox.ID}\n"       +
                           $"Height: {inBox.Height}\n" +
-                          $"Width: {inBox.Width}\n" +
+                          $"Width: {inBox.Width}\n"   +
                           $"Lenght: {inBox.Length}\n" +
-                          $"Volume: {inBox.Volume}\n"
+                          $"Volume: {inBox.Volume}\n" +
+                          $"Location: {inBox.Location}\n"
                           );
         }
 
-        static Box createBox()
+        static Box createBox(int location)
         {
             int boxID, boxHeight, boxWidth, boxLenght;
             Console.WriteLine("Please enter the Box ID: ");
@@ -48,21 +60,11 @@ namespace Lagersystem
             Console.WriteLine("Please enter the Box Lenght: ");
             boxLenght = Convert.ToInt32(Console.ReadLine());
             
-            Box myBox = new Box(boxID, boxHeight, boxWidth, boxLenght);
+            Box myBox = new Box(boxID, boxHeight, boxWidth, boxLenght, location);
 
             showBox(myBox);
             
             return myBox;
-        }
-
-        static void Menu()
-        {
-            Console.WriteLine("1. Create a Box");
-            Console.WriteLine("2. Change a Box");
-            Console.WriteLine("3. Delete a Box");
-            Console.WriteLine("4. Show a Box");
-            Console.WriteLine("5. Show all Boxes");
-            Console.WriteLine("6. Quit");
         }
 
         static void Main(string[] args)
@@ -70,31 +72,65 @@ namespace Lagersystem
             int opt, index;
             bool stop = false;
             Box[] boxes = new Box[75];
-
-            Menu();
-            opt = Convert.ToInt32(Console.ReadLine());
-            
+            for (int i = 0; i < boxes.Length; i++)
+            {
+                boxes[i] = new Box(0, i);
+            }
+            MainLoop:
             while (!stop)
             {
+                Menu.MainMenu();
+                opt = Convert.ToInt32(Console.ReadLine());
                 switch (opt)
                 {
                     case 1:
                         Console.WriteLine("Where should the Box be stored: ");
-                        index = Convert.ToInt32(Console.Read());
-                        createBox();
-                        // TODO: Add Box to Array
+                        index = Convert.ToInt32(Console.ReadLine());
+                        Box userBox = createBox(index);
+                        for (int i = 0; i < boxes.Length; i++)
+                        {
+                            if (i == index)
+                                boxes[i] = userBox;
+                        }
                         break;
                     case 4:
-                        // TODO: Show the Box.
+                        Menu.Options();
+                        opt = Convert.ToInt32(Console.ReadLine());
+                        switch (opt)
+                        {
+                            case 1:
+                                Console.WriteLine("Which box to show (ID): ");
+                                int id = Convert.ToInt32(Console.ReadLine());
+                                foreach (Box box in boxes)
+                                {
+                                    if (box.ID == id)
+                                    {
+                                        showBox(box);
+                                    }
+                                    else
+                                        Console.WriteLine("Box not in system");
+                                }
+                                break;
+                            case 2:
+                                Console.WriteLine("Where are we looking (location): ");
+                                index = Convert.ToInt32(Console.ReadLine());
+                                for (int i = 0; i < boxes.Length; i++)
+                                {
+                                    if (i == index)
+                                    {
+                                        showBox(boxes[i]);
+                                    }
+                                }
+                                break;
+                            case 3:
+                                goto MainLoop;
+                        }
+                        break;
                     case 6:
                         stop = true;
                         break;
                 }
             }
-
-            Box myBox = createBox();
-
-            Console.ReadKey();
         }
     }
 }
